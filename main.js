39c1cdda8b724d5aa2a71867b905005af5924017
Zach Nichols <zachdnichols@gmail.com>
2023-01-24 09:10:00 -0600

    Modified javascript

---
+++ main.js
@@ -0,0 +1,8 @@
+function displayNav()
+{
+    var nav = document.getElementById('nav');
+    nav.innerHTML = "<nav>Test</nav>";
+}
+
+displayNav();
+
--- nav.js
+++
@@ -1,3 +0,0 @@
-var node = document.getElementById('node-id');
-console.log(node);
-node.innerHTML = "<nav>Test</nav>";
