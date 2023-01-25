7ca76525706ec8b71119bfb5a0af224544bebde2
Zach Nichols <zachdnichols@gmail.com>
2023-01-25 14:53:47 -0600

    Added bio

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ function displayNav()
     <header> \
         <nav> \
             <ul class = "navlist"> \
-            <li class = "nav-item"><h1 id = "logo"><a class="nav-link" href="index.html">&lt;ZACH NICHOLS&gt;</a></h1></li> \
+            <li class = "nav-item"><h1 id = "logo"><a class="nav-link" href="index.html">&lt;ZACH&nbsp;NICHOLS&gt;</a></h1></li> \
+            <li class="nav-item"><a href="" class="nav-link">Projects</a></li> \
             <li class = "nav-item"><a class="nav-link" href="contact.html">Contact</a></li> \
             </ul> \
         </nav> \
