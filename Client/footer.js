c4670f7c269c9971ed8ca59cab302c92d468277d
Zach Nichols <zachdnichols@gmail.com>
2023-06-28 15:00:43 -0500

    Moved files around

--- Client/footer.js
+++
@@ -1,6 +0,0 @@
-document.write(
-    "<footer> Found a bug? Report it " +
-    "<a href='https://github.com/ZachDNichols/Personal-Website/issues/new'>here!</a>" +
-    "<br> Or might you be bored? Try out this <a href='BidensBadDay.html'>fun game</a> I made!" +
-    "</footer>"
-);
---
+++ footer.js
@@ -0,0 +1,6 @@
+document.write(
+    "<footer> Found a bug? Report it " +
+    "<a href='https://github.com/ZachDNichols/Personal-Website/issues/new'>here!</a>" +
+    "<br> Or might you be bored? Try out this <a href='/BidensBadDay'>fun game</a> I made!" +
+    "</footer>"
+);
