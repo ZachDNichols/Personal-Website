7e84a05a594a981b4dd7095d1a2e1c4a5608f4ea
Zach Nichols <zachdnichols@gmail.com>
2025-04-25 13:50:06 -0500

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit()],
+
+	build: {
+		sourcemap: true,
+	},
+});
