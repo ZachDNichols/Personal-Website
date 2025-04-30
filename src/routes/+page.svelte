68564b10132864950cdf566559463884b320904b
Zach Nichols <zachdnichols@gmail.com>
2025-04-30 10:33:18 -0500

    Started basic layout of site

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<div class="w-full flex flex-row items-center justify-center grid-cols-2 bg-gray-900 min-h-screen">
+    <img alt="Zach Nichols, the creator, owner, and maintainer of this website" src="/images/TransparentHeadshot.webp" class="max-w-1/4 m-2"/>
+    <div class="m-2 w-1/4">
+        <h1 class="font-bold w-full text-4xl">Zach Nichols</h1>
+        <p >Zach Nichols is a guy who exists in the world.</p>
+    </div>
+</div>
