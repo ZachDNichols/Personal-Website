702eca68825606393453187722b156b3021512e0
Zach Nichols <zachdnichols@gmail.com>
2025-04-30 11:13:16 -0500

    Continued to improve nav bar and added routes

--- src/components/nav/navbar.svelte
+++ src/components/nav/navbar.svelte
@@ -1,7 +1,13 @@
 <script>
     import NavItem from "./nav-item.svelte";
+    import NavSpacer from "./nav-spacer.svelte";
 </script>
 
-<nav class="bg-primary fixed w-full">
-    <NavItem title="Home" route="/" />
+<nav class="bg-primary fixed flex flex-row items-center justify-between top-4 left-4 right-4 border-10 border-primary rounded">
+    <a href="/">ZACHDNICHOLS</a>
+    <div class="justify-right">
+        <NavItem route="/contact" title="Contact"></NavItem>
+        <NavSpacer />
+        <NavItem route="/blog" title="Blog"></NavItem>
+    </div>
 </nav>
