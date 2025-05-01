36261d5ff7eda0bafe05b2037b56ec7828a16e97
Zach Nichols <zachdnichols@gmail.com>
2025-05-01 18:13:20 -0500

    Update video embedding and redesign layout
    Revised video embedding to use YouTube embed URLs and updated the video display structure for improved responsiveness and visual consistency. Adjusted layout styles, including margin and width updates, and removed unnecessary padding. Added a new screenshot for documentation purposes.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,6 @@
 <Navbar />
 <div class="fixed min-h-screen min-w-screen -z-50 bg-gray-700"></div>
 
-<main class="pt-20 flex flex-col items-center justify-center">
+<main class="flex flex-col items-center justify-center">
     {@render children()}
 </main>
