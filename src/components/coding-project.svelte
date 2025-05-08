b2b49b232e03e76efdb24fcf96a49a460de015e7
Zach Nichols <zachdnichols@gmail.com>
2025-05-08 17:33:53 -0500

    "Mobile First" site design
    Having to redo the styling essentially twice is making a fella question his sanity

--- src/components/coding-project.svelte
+++ src/components/coding-project.svelte
@@ -4,10 +4,10 @@
     let { title, description, image, url, caption = "" } = $props();
 </script>
 
-<div class="flex flex-col items-center justify-items-start mt-10 mr-10 ml-10 w-1/2">
+<div class="flex flex-col items-center justify-items-start mt-5 mr-10 ml-10 w-11/12">
     <h3 class="text-2xl font-bold">{title}</h3>
-    <p class="text-center">{description}</p>
-    <img src={image} onerror="{handleImageError}" alt={title} class="justify-center shadow-2xl rounded" />
-    <p class="text-center text-sm">{caption}</p>
-    <a href="{url}" class="mb-4 text-white bg-primary rounded-xl p-2 transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl hover:font-bold hover:translate-y-[-1.5] m-4">View on GitHub</a>
+    <p class="text-center bg-gray-800 rounded-2xl w-full m-2 p-2">{description}</p>
+    <img src={image} onerror="{handleImageError}" alt={title} class="justify-center shadow-lg rounded" />
+    <p class="text-center text-xs mb-1">{caption}</p>
+    <a href="{url}" class="text-sm mb-4 p-1 transition-all duration-300 ease-in-out underline">View on GitHub</a>
 </div>
