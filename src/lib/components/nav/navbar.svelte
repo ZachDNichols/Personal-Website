229a374360313668e516f11bda46ae91386429c1
Zach Nichols <zachdnichols@gmail.com>
2025-05-12 22:27:46 -0500

    Minor tweaks

--- src/lib/components/nav/navbar.svelte
+++ src/lib/components/nav/navbar.svelte
@@ -1,4 +1,4 @@
-<nav class="fixed flex flex-row items-center justify-center top-0 left-0 w-full h-16 backdrop-blur-3xl z-50 md:justify-between">
+<nav class="fixed flex flex-row items-center justify-center top-0 left-0 w-full h-16 backdrop-blur-3xl z-50 lg:justify-between">
     <div class="bg-primary fixed w-full opacity-75 h-16 border-10 border-primary shadow-xl "></div>
-    <p class="m-4 z-50 text-2xl font-bold text-white transition-all relative after:absolute after:bottom-0 after:left-0 after:h-[.2rem] after:w-full after:origin-bottom-right after:bg-white after:transition-transform after:duration-300 hover:after:origin-bottom-left after:rounded-xs text-center select-none">ZACHDNICHOLS.COM</p>
+    <p class="m-4 z-50 text-2xl font-bold text-white transition-all relative select-none">ZACHDNICHOLS.COM</p>
 </nav>
