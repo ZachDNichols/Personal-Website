68454446785827fa09896836ff67062c80910b59
Zach Nichols <zachdnichols@gmail.com>
2025-05-13 10:32:43 -0500

    Error page

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,5 +1,11 @@
-<footer class="flex flex-col items-center justify-center w-full h-16 bg-primary text-white">
-    <p class="w-11/12 text-center md:w-full"> Found a bug or an issue? Report a problem on 
-        <a class="font-bold select-none transition-all hover:scale-105 relative after:absolute after:bottom-0 after:left-0 after:h-[.15rem] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-white after:transition-transform after:duration-300 hover:after:origin-bottom-left hover:after:scale-x-100 after:rounded-2xl" href="https://github.com/ZachDNichols/Personal-Website/issues">Github</a>
-    </p>
-</footer>
+<script>
+    import { page }  from '$app/state'
+</script>
+
+{#if page.status === 200}
+    <footer class="flex flex-col items-center justify-center w-full h-16 bg-primary text-white">
+        <p class="w-11/12 text-center md:w-full"> Found a bug or an issue? Report a problem on 
+            <a class="font-bold select-none transition-all hover:scale-105 relative after:absolute after:bottom-0 after:left-0 after:h-[.15rem] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-white after:transition-transform after:duration-300 hover:after:origin-bottom-left hover:after:scale-x-100 after:rounded-2xl" href="https://github.com/ZachDNichols/Personal-Website/issues">Github</a>
+        </p>
+    </footer>
+{/if}
